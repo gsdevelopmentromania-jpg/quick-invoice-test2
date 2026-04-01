ced0048b5fd90fbc304253733c4d75b920a624ef
gsdevelopmentromania-jpg <gsdevelopment.romania@gmail.com>
2026-04-01 08:37:33 +0300

    feat: add GlobalShortcuts to dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,17 @@
 import { DashboardShell } from "@/components/layout/dashboard-shell";
+import { GlobalShortcuts } from "@/components/global-shortcuts";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-export default function DashboardLayout({ children }: DashboardLayoutProps): React.ReactElement {
-  return <DashboardShell>{children}</DashboardShell>;
+export default function DashboardLayout({
+  children,
+}: DashboardLayoutProps): React.ReactElement {
+  return (
+    <DashboardShell>
+      <GlobalShortcuts />
+      {children}
+    </DashboardShell>
+  );
 }
